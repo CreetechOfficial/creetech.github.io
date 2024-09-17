c93e17d9add95319908b2b1a6dc75f90c64967c4
Jeffri Andriyanto <jeff.tm07@gmail.com>
2024-09-17 13:37:36 +0700

    first commit

---
+++ assets/home.1c6fba51.js
@@ -0,0 +1,1 @@
+import{g as o,r as n,o as t,c,b as r}from"./app.d6551d1e.js";const s={};function a(_,m){const e=n("RouterView");return t(),c("main",null,[r(e)])}const f=o(s,[["render",a]]);export{f as default};
